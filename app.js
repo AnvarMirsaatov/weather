d199b049c2fb138b08a50634c8f350101c6348fd
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2026-08-05 09:55:41 +0500

    adsfasdf

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-let city = localStorage.getItem("city")
-  ? ocalStorage.getItem("city")
-  : "tashkent";
-let api = `http://api.weatherapi.com/v1/current.json?key=b7103d4704784776984135054260408&q=${city}&aqi=yes`;
+function getWeather() {
+  const city = localStorage.getItem("city")
+    ? localStorage.getItem("city")
+    : "Toshkent";
 
-fetch(api)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+  let api = `http://api.weatherapi.com/v1/current.json?key=be32b263b40045d4bf641850260508&q=${city}&aqi=yes `;
 
+  fetch(api)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      render(data);
+    });
+}
+getWeather();
 
-  
+function render(data) {
+  document.querySelector(".city-name").innerHTML = data.location.name;
+  document.querySelector(".date-text").innerHTML = data.location.localtime;
+  document.querySelector(".main-temp").innerHTML = data.current.temp_c;
+  document.querySelector(".temp-unit").innerHTML = "°C";
+  document.querySelector(".weather-desc").innerHTML =
+    data.current.condition.text;
+}
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  let input = document.querySelector(".enter_city_input").value;
+  localStorage.setItem("city", input);
+});
